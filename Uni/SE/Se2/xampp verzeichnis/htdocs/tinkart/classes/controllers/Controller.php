6dcbe9e2f3afc91bfa4a9a80dbe4bc0b2d3e7df9
Michali <m.alexandrakis@web.de>
2014-11-20 23:23:02 +0100

    huhukjnkj

---
+++ Uni/SE/Se2/xampp verzeichnis/htdocs/tinkart/classes/controllers/Controller.php
@@ -0,0 +1,74 @@
+<?php
+
+class Controller {
+
+    public function display($query)
+    {
+        $layout = View::make('layout');
+
+        switch($query)
+        {
+            case '':
+                echo $layout->set('title', 'Tinkart')->set('content', $this->index());
+                break;
+
+            case '/projectpage':
+                echo $layout->set('title', 'Tinkart - Project')->set('content', $this->projectpage());
+                break;
+
+            case '/register':
+                echo $layout->set('title', 'Registration')->set('content', $this->registrationPage());
+                break;
+            case '/searchresult':
+                echo $layout->set('title', 'Searchresults')->set('content', $this->searchresults());
+                break;
+
+            case '/create-project':
+                echo $layout->set('title', 'Project Creator')->set('content', $this->createProject());
+                break;
+
+
+            default:
+                echo '404 Diese Seite wurde nicht gefunden';
+                break;
+        }
+    }
+
+    public function index()
+    {
+        $project = new Project();
+        //$comments = new Comment();
+
+        $projects = $project->all();
+
+        foreach ($projects as &$pro)
+        {
+           // $pro['description'] = 'blub';
+            //$pro['comments'] = $comments->allByProject($pro['id']);
+        }
+
+        return View::make('startseite')->set([
+                'title' => 'Tinkart',
+                'heading' => 'global ideas for everyone ',
+                'projects' => $projects
+            ]);
+    }
+
+    public function projectPage()
+    {
+        return View::make('projectpage');
+    }
+    public function registrationPage()
+    {
+        return View::make('register');
+    }
+    public function searchresults()
+    {
+        return View::make('searchresult');
+    }
+    public function createProject()
+    {
+        return View::make('create-project');
+    }
+
+} 
---
+++ Uni/SE/Se2/xampp verzeichnis/htdocs/tinkart/templates/create-project.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: PengBang
+ * Date: 12.11.2014
+ * Time: 22:08
+ */ 
