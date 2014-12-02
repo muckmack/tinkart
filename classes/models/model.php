209f023672dac19ecef430d38b7b104f63962ad5
Michali <m.alexandrakis@web.de>
2014-12-03 00:36:41 +0100

    Texte in den newes und best projektboxen geändert

--- classes/models/model.php
+++ classes/models/model.php
@@ -13,28 +13,28 @@ class model {
         $link = mysql_connect('localhost', 'admin', '');
         if(!$link){
             die("kein server zugriff/gefunden");
-        }
-        mysql_select_db('mydb') or die("datenbank nicht gefunden");
-        return $link;
     }
+mysql_select_db('mydb') or die("datenbank nicht gefunden");
+return $link;
+}
 
-    public static function orderProjectsByTitle($uid){
-        $link = self::openDataBase();
-        $sql = "SELECT pid FROM project ORDER BY proj_title ASC;";
-        $result=mysql_query($sql);
-        if (!$result){
-            echo mysql_error();
-        }
-        $orderedList = array("pid");
-        while ($row = mysql_fetch_array($result)){
-            $orderedList[] = $row;
-
-        }
-        //erste zeile entfernen
-        unset($orderedList[0]);
-        mysql_close($link);
-        return $orderedList;
+public static function orderProjectsByTitle($uid){
+    $link = self::openDataBase();
+    $sql = "SELECT pid FROM project ORDER BY proj_title ASC;";
+    $result=mysql_query($sql);
+    if (!$result){
+        echo mysql_error();
     }
+    $orderedList = array("pid");
+    while ($row = mysql_fetch_array($result)){
+        $orderedList[] = $row;
+
+    }
+    //erste zeile entfernen kaka
+    unset($orderedList[0]);
+    mysql_close($link);
+    return $orderedList;
+}
     public static function getProjectVote($pid){
         $link = self::openDataBase();
         $sql = "SELECT P_VOTE_UP, P_VOTE_DOWN FROM project WHERE PID=".$pid;
